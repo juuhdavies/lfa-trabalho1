3a2f0a3bd9a8a758c93560ec8da21b373d0ba209
Julia Davies <62300527+juuhdavies@users.noreply.github.com>
2025-07-20 18:47:17 -0300

    Update afd.cpp

--- afd.cpp
+++ afd.cpp
@@ -1,3 +1,69 @@
 #include "afd.h"
+#include <algorithm>
+#include <iostream>
 
-afd::afd() {}
+using namespace std;
+
+afd::afd() {
+    this->estado_inicial = "q0";
+}
+
+//adicionar simbolo no alfabeto
+void afd::add_simbolo(char s){
+    this->alfabeto.push_back(s);
+}
+
+void afd::add_estado(const string& estado){
+    this->estados.push_back(estado);
+}
+
+void afd::add_estado_final(const string& estado){
+    this->estados_finais.push_back(estado);
+}
+
+void afd::add_transicao(const transicao& t){
+    this->transicoes.push_back(t);
+}
+
+bool afd::eh_estado_final(const string& estado) const{
+    return find(estados_finais.begin(), estados_finais.end(), estado) != estados_finais.end();
+}
+
+string afd::prox_estado(const string& estado_atual, char s) const{
+    for(const auto& t : transicoes){
+        if(t.estado_origem == estado_atual && t.simbolo == s){
+            return t.estado_destino;
+        }
+    }
+    
+    return "";
+}
+
+//pode tirar dps mas usar para ver se o afd foi lido corretamente
+void afd::mostrar(){
+    
+    cout << "Alfabeto: ";
+    for(char c : alfabeto){
+        cout << c << " ";
+    }
+    cout << endl;
+    
+    cout << "Estados: ";
+    for(const auto& e : estados){
+        cout << e << " ";
+    }
+    
+    cout << endl << "Estado Inicial: " << estado_inicial << endl;
+    
+    cout << "Estados Finais: ";
+    for(const auto& e : estados_finais){
+        cout << e << " ";
+    }
+    cout << endl;
+    
+    cout << "Transicoes: ";
+    for(const auto& t : transicoes){
+        t.mostrar();
+    }
+    
+}
