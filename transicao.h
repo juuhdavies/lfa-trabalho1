49f30b814d26f72eb96f08722d8374eb692d95da
Julia Davies <62300527+juuhdavies@users.noreply.github.com>
2025-07-20 18:11:46 -0300

    Update transicao.h

--- transicao.h
+++ transicao.h
@@ -12,6 +12,9 @@ public:
     char simbolo;
     string estado_origem;
     string estado_destino;
+
+    transicao(char s, const string& orig, const string& dest);
+    void mostrar() const;
 };
 
 #endif // TRANSICAO_H
