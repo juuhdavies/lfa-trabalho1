d861f196de4eb76ad797ced7ac79eab43c88cb54
Julia Davies <62300527+juuhdavies@users.noreply.github.com>
2025-07-20 20:25:58 -0300

    Update afd.h

--- afd.h
+++ afd.h
@@ -1,6 +1,12 @@
 #ifndef AFD_H
 #define AFD_H
 
+#include <vector>
+#include <string>
+#include "transicao.h"
+
+using namespace std;
+
 class afd
 {
 private:
@@ -10,21 +16,22 @@ private:
     vector<transicao> transicoes;
     string estado_inicial;
 public:
-    
+
     //Construtor
     afd();
-    
+
     //usar quando for ler os estados do arquivo para armazenar
     void add_simbolo(char s);
     void add_estado(const string& estado);
     void add_estado_final(const string& estado);
     void add_transicao(char s, string& origem, string& destino);
-    
+
     bool eh_estado_final(const string& estado) const;
     string prox_estado(const string& estado_atual, char s) const;
-    
+
     void mostrar();
-    
+
+
 };
 
 #endif // AFD_H
