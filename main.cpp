be714e52d3a3d9c2f168a70218b195edb3a5a5e3
Julia Davies <62300527+juuhdavies@users.noreply.github.com>
2025-07-20 19:41:34 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,46 @@
 #include <iostream>
 #include "afd.h"
 #include <fstream>
+#include <string>
+#include <istream>
 #include <algorithm>
 
 using namespace std;
 
 void ler_linha(const string& linha, afd& automato){
-    if(linha.find("alfabeto")){
-        
+    
+    //busca alfabeto na linha, string::npos é usada para inficar que a busca não encontrou oq estava procurando
+    if(linha.find("alfabeto") != string::npos){
+
     }
 }
 
 
-//não sei se vai ser necessário provavelmente da pra fazer tudo na main e ler_linha
-void ler_afd(){
-    
+
+void ler_afd(const string& nome_arquivo, afd& automato){
+    ifstream file(nome_arquivo);
+    if(!file.is_open()){
+        cerr << "Erro ao abrir arquivo" << endl;
+        exit(1);
+    }
+
+    string linha;
+    while(getline(file, linha)){
+        ler_linha(linha, automato);
+    }
+
+
 }
 
 int main()
 {
     afd automato;
     string nome_arquivo;
-    
-    
+
+
     //por hora deixar só um arquivo e dps de ler usar o método de mostrar afd pra conferir
     //Fazer um negócio lá de perguntar oq o usuário quer fazer ex: imprimir gramática, processar cadeia.
     cin >> nome_arquivo;
-    
-    ifstream file(nome_arquivo);
-    if(!file.is_open()){
-        cout << "Erro ao abrir arquivo" << endl;
-        return 0;
-    }
+
+
 }
