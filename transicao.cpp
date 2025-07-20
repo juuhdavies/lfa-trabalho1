0ade44ca9dff43f87af04d652471725a3bce5b67
Julia Davies <62300527+juuhdavies@users.noreply.github.com>
2025-07-20 18:53:53 -0300

    Update transicao.cpp

--- transicao.cpp
+++ transicao.cpp
@@ -1,3 +1,15 @@
 #include "transicao.h"
+#include <iostream>
 
-transicao::transicao() {}
+using namespace std;
+
+transicao::transicao(char s, const string& orig, const string& dest) {
+    this->simbolo = s;
+    this->estado_origem = orig;
+    this->estado_destino = dest;
+}
+
+
+void transicao::mostrar() const{
+    cout << "(" << estado_origem << "," << estado_destino << ")" << endl;
+}
