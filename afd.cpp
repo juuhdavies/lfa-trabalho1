864e79ce7e2a8c0e924e029b7f216f88cc38283e
arthurgsmarques <agsmarques@gmail.com>
2025-07-22 22:10:42 -0300

    Update afd.cpp

--- afd.cpp
+++ afd.cpp
@@ -39,35 +39,66 @@ string afd::prox_estado(const string& estado_atual, char s) const{
             return t.estado_destino;
         }
     }
-    
+
     return "";
 }
 
 //pode tirar dps mas usar para ver se o afd foi lido corretamente
 void afd::mostrar(){
-    
+
     cout << "Alfabeto: ";
     for(char c : alfabeto){
         cout << c << " ";
     }
     cout << endl;
-    
+
     cout << "Estados: ";
     for(const auto& e : estados){
         cout << e << " ";
     }
-    
+
     cout << endl << "Estado Inicial: " << estado_inicial << endl;
-    
+
     cout << "Estados Finais: ";
     for(const auto& e : estados_finais){
         cout << e << " ";
     }
     cout << endl;
-    
+
     cout << "Transicoes: ";
     for(const auto& t : transicoes){
         t.mostrar();
     }
-    
+
+}
+
+void afd::processa_cadeia(){
+    string palavra;
+
+    cout << "Palavra para processar: ";
+    cin >> palavra;
+
+    cout << "[" + estado_inicial + "]" + palavra << endl;
+
+    if(palavra=="@"){
+        if(eh_estado_final(estado_inicial)) cout << "ACEITA" << endl;
+        else cout << "REJEITA" << endl;
+    }
+    else{
+        bool aceita = true;
+        string estado_atual = estado_inicial;
+
+        while(!palavra.empty()&&aceita){
+            char letra = palavra[0];
+            palavra.erase(0,1);
+            estado_atual = prox_estado(estado_atual,letra);
+
+            if(estado_atual.empty()) aceita = false;
+            else{
+                cout << "[" + estado_atual + "]" + palavra << endl;
+                if(palavra.empty()&&!eh_estado_final(estado_atual)) aceita = false;
+            }
+        }
+        aceita? cout<<"ACEITA"<<endl : cout<<"REJEITA"<<endl;
+    }
 }
